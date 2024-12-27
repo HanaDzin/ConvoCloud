f340359519f3cb255a7e42b943e23d4ef7c15546
Hana <dzinhana@hotmail.com>
2024-12-27 13:45:35 +0100

    add getMessages and sendMessage functions

--- backend/src/controllers/messageController.js
+++ backend/src/controllers/messageController.js
@@ -1,4 +1,5 @@
-import User from "../models/userModel";
+import User from "../models/userModel.js";
+import Message from "../models/messageModel.js";
 
 // fetch all users from the database, excluding the currently logged in, to show in the sidebar
 export const getUsersForSidebar = async (req, res) => {
@@ -13,5 +14,51 @@ export const getUsersForSidebar = async (req, res) => {
   } catch (error) {
     console.log("Error in getUsersForSidebar: ", error.message);
     res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { receiverId } = req.params; // passed via the route
+    const senderId = req.user._id; // from protectRoute
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: senderId, receiverId: receiverId },
+        { senderId: receiverId, receiverId: senderId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in the getMessages controller: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { receiverId } = req.params;
+    const senderId = req.user._id;
+
+    // ADD LOGIC TO SEND A PICTURE
+
+    // create the new message
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    // REAL-TIME FUNCTIONALITY - SOCKET.IO
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in the sendMessage controller: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
--- backend/src/routes/messageRoutes.js
+++ backend/src/routes/messageRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { getUsersForSidebar } from "../controllers/messageController.js";
+import {
+  getUsersForSidebar,
+  getMessages,
+  sendMessage,
+} from "../controllers/messageController.js";
 import { protectRoute } from "../middleware/authMiddleware.js";
 
 const messageRouter = express.Router();
 
 messageRouter.get("/users", protectRoute, getUsersForSidebar);
+
+messageRouter.get("/:receiverId", protectRoute, getMessages);
+
+messageRouter.post("send/:receiverId", protectRoute, sendMessage);
 
 export default messageRouter;
